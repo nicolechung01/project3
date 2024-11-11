efdaf67e6a092a1f6c83a3e971581edbb4595af7
Nicole Chung <nicolechung01@gmail.com>
2024-11-11 18:21:37 -0500

    directory update

---
+++ run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set the source and output directories
+SRC_DIR="src/main/java"
+BIN_DIR="bin"
+LIB_DIR="lib"
+
+# Create the bin directory if it doesn't exist
+mkdir -p $BIN_DIR
+
+# Compile Java files
+echo "Compiling Java files..."
+javac -d $BIN_DIR -sourcepath $SRC_DIR -cp "$LIB_DIR/*" $SRC_DIR/*.java
+
+
