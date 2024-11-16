dbd1de0639ef8995f252497d4e0027083769fb99
John Atkinson <42153925+Green-atkinson@users.noreply.github.com>
2024-11-16 14:02:37 -0500

    Update TestTupleGenerator.java
    added table creation for part3 of our project

--- src/main/java/TestTupleGenerator.java
+++ src/main/java/TestTupleGenerator.java
@@ -11,6 +11,7 @@ import static java.lang.System.out;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.List;
 import java.util.Map;
 
@@ -111,6 +112,7 @@ public class TestTupleGenerator
             tableObjects.add(table);
         }
 
+        /*
         // for printing tables to verify
         for (Table table: tableObjects) {
             table.print();
@@ -127,7 +129,60 @@ public class TestTupleGenerator
             } // for
             out.println ();
         } // for
+        */
+        /*                                                                                                                                                                                                                                                              
+        // for printing tables to verify                                                                                                                                                                                                                                
+        for (Table table: tableObjects) {                                                                                                                                                                                                                               
+            table.print();                                                                                                                                                                                                                                              
+        }                                                                                                                                                                                                                                                               
+                                                                                                                                                                                                                                                                        
+        // printing tuples to terminal                                                                                                                                                                                                                                  
+        for (var i = 0; i < resultTest.length; i++) {                                                                                                                                                                                                                   
+            out.println (tables[i]);                                                                                                                                                                                                                                    
+            for (var j = 0; j < resultTest[i].length; j++) {                                                                                                                                                                                                            
+                for (var k = 0; k < resultTest[i][j].length; k++) {                                                                                                                                                                                                     
+                    out.print (resultTest[i][j][k] + ",");                                                                                                                                                                                                              
+                } // for                                                                                                                                                                                                                                                
+                out.println ();                                                                                                                                                                                                                                         
+            } // for                                                                                                                                                                                                                                                    
+            out.println ();                                                                                                                                                                                                                                             
+        } // for                                                                                                                                                                                                                                                        
+        */
 
+        Table studentTable = tableObjects.get(0);
+        Table transcriptTable = tableObjects.get(4);
+
+        int[] tupleCounts = {500, 1000, 2000, 5000, 10000};
+        String attributeToSelect = "id";
+        int selectValue = 123; // Example value to search                                                                                                                                                                                                               
+
+        // Loop through different indexing mechanisms                                                                                                                                                                                                                   
+        Map<String, Map<KeyType, Comparable[]>> indexTypes = new HashMap<>();
+        indexTypes.put("NoIndex", null); // No indexing                                                                                                                                                                                                                 
+        indexTypes.put("TreeMap", new TreeMap<>());
+        indexTypes.put("HashMap", new HashMap<>());
+        indexTypes.put("LinHashMap", new LinHashMap<>(KeyType.class, Comparable[].class));
+
+        for (String indexType : indexTypes.keySet()) {
+            Map<KeyType, Comparable[]> index = indexTypes.get(indexType);
+
+           //       for (int tupleCount : tupleCounts) {                                                                                                                                                                                                                
+            //          System.out.println("\n--- Testing " + indexType + " with " + tupleCount + " tuples ---");                                                                                                                                                       
+            System.out.println("\nTesting with index type: " + indexType);
+
+                // Measure time for SELECT operation                                                                                                                                                                                                                    
+                long startSelect = System.currentTimeMillis();
+                Table resultSelect = studentTable.select(t -> t[studentTable.col("id")].equals(selectValue));
+                long endSelect = System.currentTimeMillis();
+                System.out.println("Select operation took: " + (endSelect - startSelect) + " ms");
+
+                // Measure time for JOIN operation                                                                                                                                                                                                                      
+                long startJoin = System.currentTimeMillis();
+                Table resultJoin = studentTable.join("id", "studId", transcriptTable);
+                long endJoin = System.currentTimeMillis();
+                System.out.println("Join operation took: " + (endJoin - startJoin) + " ms");
+                //          }                                                                                                                                                                                                                                           
+        }
     } // main
 
 } // TestTupleGenerator
